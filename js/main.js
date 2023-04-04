82748c0b3bff8c4cc8b4be453b157ee15eba4956
Dinara <dinara.baqyt@gmail.com>
2023-04-04 13:27:23 +0600

    jss-if,else

--- js/main.js
+++ js/main.js
@@ -1,20 +1,57 @@
- const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+// let num = 90;
+// if (num < 12) {
+//     console.log("Больше");
+// } else if(num > 100) {
+//     console.log("Меньше");
+// } else {
+//     console.log(90);
+// }
 
-  const personalMovieDB = {
-      'count': numberOfFilms,
-      'movies': {},
-      'actors': {},
-      'genres': [],
-      'privat': false
-     };
+// const num = 50;
+// (num === 50) ? (console.log("Ok")) : (console.log("Error"));
+
+let num = 30;
+switch (num) {
+    case 32:
+        console.log('Error');
+        break;
+    case 40:
+        console.log('Меньше');
+        break;
+    case 30:
+        console.log('Правильно!')
+        break;
+}
+
+let a = "I go to school";
+switch (a) {
+    case "I went at school":
+        console.log("Nope");
+        break;
+    case "I am from China":
+        console.log("Nope");
+        break;
+    case "I go school":
+        console.log("OK!");
+        break;
+        default:
+            console.log("Не в этот раз");
+            break;
+}
+
+const audi = true;
+const bmv = false;
+
+if (audi || bmv) {
+    console.log('AUDI');
+}
 
 
-let ques1 = prompt("Один из последних просмотренных фильмов?", ''),
-    ques = prompt("На сколько оцените его?", '');
-    ans1 = prompt("Один из последних просмотренных фильмов?", ''),
-    ans = prompt("На сколько оцените его?", '');
+let dinaReport, aliReport, annaReport = "done";
+console.log(dinaReport || aliReport || annaReport);
 
-    personalMovieDB.movies[ques1] = ques;
-    personalMovieDB.movies[ans1] = ans;
-
-console.log(personalMovieDB);
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+console.log(hamburger ===3 && (cola == 2 || fries == 3) && nuggets);
