cdfbdf5f702f5e7cadb970675d1ec5e74a7dc667
Dinara <dinara.baqyt@gmail.com>
2023-04-07 15:06:12 +0600

    main.js changes about function part

--- js/main.js
+++ js/main.js
@@ -1,57 +1,68 @@
-// let num = 90;
-// if (num < 12) {
-//     console.log("Больше");
-// } else if(num > 100) {
-//     console.log("Меньше");
-// } else {
-//     console.log(90);
+'use strict'
+//  let numberOfFilms;
+
+//  function start() {
+//      numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
+
+//      while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//          numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
+//      }
+//  }
+//  start();
+
+let personalMovieDB = {
+    count: {},
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+//  function rememberMyFilms() {
+//     for(let i = 0; i < 2; i++) {
+//         const a = prompt("Один из последних просмотренных фильмов?", ""),
+//               b = prompt("На сколько оцените его?", "");
+        
+//         if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done!');
+//         } else {
+//             console.log('Error');
+//             i--;
+//         }
+//     }
+// }
+// rememberMyFilms();
+
+//  function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log("Просмотрено довольно мало фильмов");
+//     } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log("Вы классический зритель");
+//     } else if(personalMovieDB.count >= 30) {
+//         console.log("Вы киноман");
+//     } else {
+//         console.log("Произошла ошибка");
+//     }
 // }
 
-// const num = 50;
-// (num === 50) ? (console.log("Ok")) : (console.log("Error"));
+// detectPersonalLevel();
 
-let num = 30;
-switch (num) {
-    case 32:
-        console.log('Error');
-        break;
-    case 40:
-        console.log('Меньше');
-        break;
-    case 30:
-        console.log('Правильно!')
-        break;
+ function showMyDB(hidden) {
+     if(!hidden) {
+         console.log(personalMovieDB);
+     }
+ };
+ showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+   for( let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+   }
 }
-
-let a = "I go to school";
-switch (a) {
-    case "I went at school":
-        console.log("Nope");
-        break;
-    case "I am from China":
-        console.log("Nope");
-        break;
-    case "I go school":
-        console.log("OK!");
-        break;
-        default:
-            console.log("Не в этот раз");
-            break;
-}
-
-const audi = true;
-const bmv = false;
-
-if (audi || bmv) {
-    console.log('AUDI');
-}
+writeYourGenres();
 
 
-let dinaReport, aliReport, annaReport = "done";
-console.log(dinaReport || aliReport || annaReport);
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
-console.log(hamburger ===3 && (cola == 2 || fries == 3) && nuggets);
